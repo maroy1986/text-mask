3bb87b6ad7a88fde77ca2b6ba994fe555637f546
Marc-Andre Roy <maroy@omniscient.ca>
2017-03-02 12:19:14 -0500

    Drop some code here for later on.

--- aurelia/src/aureliaTextMask.ts
+++ aurelia/src/aureliaTextMask.ts
@@ -19,3 +19,41 @@ export class MaskInput {
         
     }
 }
+
+import { autoinject, customAttribute, bindable, bindingMode } from 'aurelia-framework';
+import { createTextMaskInputElement } from 'textMaskCore';
+
+// @customAttribute('phone-mask')
+// @autoinject()
+// export class PhoneMask {
+//   @bindable({
+//     defaultBindingMode: bindingMode.twoWay, changeHandler: 'unMaskedChanged'
+//   }) public unMasked: string;
+//   public mask: string = '[1-9]';
+//   private textMaskInputElement: any;
+
+//   constructor(private element: Element, test: string) {
+//     if (!(element instanceof HTMLInputElement)) {
+//       throw new Error(`text-mask attribute can only be applied to input element.`);
+//     }
+
+//     this.textMaskInputElement = createTextMaskInputElement(
+//       Object.assign({ inputElement: this.element }, {
+//         // mask: '[\'(\', /[1-9]/, /\d/, /\d/, \')\', \' \', /\d/, /\d/, /\d/, \'-\', /\d/, /\d/, /\d/, /\d/]',
+//         mask: ['(', /\d/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, ' ', '-', ' ', /\d/, /\d/, /\d/, /\d/],
+//         guide: true,
+//         placeholderChar: '_',
+//         pipe: undefined,
+//         keepCharPositions: true,
+//       })
+//     );
+
+//   }
+
+//   public unMaskedChanged(newValue: string, oldValue: string): void {
+//     console.info(`Hey my value changed from '${oldValue}' to '${newValue}'.`);
+//     if (this.textMaskInputElement !== undefined) {
+//       this.textMaskInputElement.update(newValue);
+//     }
+//   }
+// }
